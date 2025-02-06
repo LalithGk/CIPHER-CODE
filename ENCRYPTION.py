92673ceaafc215ca38e001619fe4a1d346c541e5
Lalith gk <LalithGk>
2025-02-06 15:02:24 +0530

    Adding Decryption

--- ENCRYPTION.py
+++ ENCRYPTION.py
@@ -1,25 +1,41 @@
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-code = input("Enter the message: ").lower()
-
-morsecode = []
-finalcode = ""
-steps = int(input("Enter the gap: "))
-
-for j in code:
-    if j == " ":  # Handle spaces directly
-        morsecode.append(" ")
-    else:
-        for i in range(0, len(letters)):
-            if letters[i] == j:
-                if i + steps >= 26:
-                    a = 26 - i
-                    morsecode.append(letters[steps - a])
-                else:
-                    morsecode.append(letters[i + steps])
-
-print(morsecode)
-
-for k in morsecode:
-    finalcode = finalcode + k
-
-print(finalcode)
+def encript(code,steps):
+    code1= code.lower()
+    morsecode = []
+    finalcode = ""
+    for j in code1:
+        if j == " ":  
+            morsecode.append(" ")
+        else:
+            for i in range(0, len(letters)):
+                if letters[i] == j:
+                    if i + steps >= 26:
+                        a = 26 - i
+                        morsecode.append(letters[steps - a])
+                    else:
+                        morsecode.append(letters[i + steps])
+    for k in morsecode:
+        finalcode = finalcode + k
+    
+    print(finalcode) 
+    
+def decript(code,steps):
+    code1 = code.lower()
+    morsecode = []
+    finalcode = ""
+    for j in code1:
+        if j == " ":
+            morsecode.append(" ")
+        else:
+            for i in range(0, len(letters)):
+                if letters[i] == j:
+                    if i - steps <= 0:
+                        a = 26 - i
+                        morsecode.append(letters[a-1])
+                    else:
+                        morsecode.append(letters[i - steps])
+    for k in morsecode:
+        finalcode = finalcode + k
+    
+    print(finalcode)
+encript("z i", 2)
