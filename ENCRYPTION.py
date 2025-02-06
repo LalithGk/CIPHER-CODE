fc3d171e62419d862210490c14676d046408f07a
Lalith gk <LalithGk>
2025-02-06 14:16:38 +0530

    Handling the spaces

--- ENCRYPTION.py
+++ ENCRYPTION.py
@@ -1,24 +1,25 @@
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-while True:
-    code = input("Enter the message: ").lower()
-    if " " in code:
-        print("Invalid Try without spaces: ")
-        continue
-    break
+code = input("Enter the message: ").lower()
 
 morsecode = []
 finalcode = ""
 steps = int(input("Enter the gap: "))
-for i in range(0,len(letters)):
-    for j in code:
-        if letters[i] == j:
-            if i + steps >= 26:
-                a = 26 - i
-                morsecode.append(letters[steps - a])
-            else:
 
-                morsecode.append(letters[i+steps])
+for j in code:
+    if j == " ":  # Handle spaces directly
+        morsecode.append(" ")
+    else:
+        for i in range(0, len(letters)):
+            if letters[i] == j:
+                if i + steps >= 26:
+                    a = 26 - i
+                    morsecode.append(letters[steps - a])
+                else:
+                    morsecode.append(letters[i + steps])
+
+print(morsecode)
+
 for k in morsecode:
     finalcode = finalcode + k
+
 print(finalcode)
