ee6416c388b2cb0a9987859cc4d5a54560041222
Lalith gk <LalithGk>
2025-02-06 16:30:44 +0530

    Final

--- ENCRYPTION.py
+++ ENCRYPTION.py
@@ -1,42 +1,44 @@
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-def encript(code,steps):
-    code1= code.lower()
-    morsecode = []
+text =  input("Enter the text: ")
+leap = int(input("Enter the Leap of alphabets: "))
+def encript(text,leap):
+    code1= text.lower()
+    code = []
     finalcode = ""
     for j in code1:
         if j == " ":  
-            morsecode.append(" ")
+            code.append(" ")
         else:
             for i in range(0, len(letters)):
                 if letters[i] == j:
-                    if i + steps >= 26:
+                    if i + leap >= 26:
                         a = 26 - i
-                        morsecode.append(letters[steps - a])
+                        code.append(letters[leap - a])
                     else:
-                        morsecode.append(letters[i + steps])
-    for k in morsecode:
+                        code.append(letters[i + leap])
+    for k in code:
         finalcode = finalcode + k
     
     print(finalcode) 
     
-def decrypt(code,steps):
-    code1 = code.lower()
-    morsecode = []
-    finalcode = ""
+def decrypt(text,leap):
+    code1 = text.lower()
+    real_word = []
+    final_decrypt = ""
     for j in code1:
         if j == " ":
-            morsecode.append(" ")
+            real_word.append(" ")
         else:
             for i in range(0, len(letters)):
                 if letters[i] == j:
-                    if i - steps <= 0:
+                    if i - leap <= 0:
                         a = 26 - i
-                        morsecode.append(letters[a-1])
+                        real_word.append(letters[a-1])
                     else:
-                        morsecode.append(letters[i - steps])
-    for k in morsecode:
-        finalcode = finalcode + k
+                        real_word.append(letters[i - leap])
+    for k in real_word:
+        final_decrypt = final_decrypt + k
     
-    print(finalcode)
-encript("z i", 2)
-decrypt("a", 1)
+    print(final_decrypt)
+# encript(actual_text, leap)
+decrypt(text, leap)
